eb7e09c0d409ba85024f241b629d4ff9fc71d012
guptadeepanshi <deepanshigupta@neepanlok.com>
2026-02-05 15:45:35 +0530

    minor changes

--- lib/screens/pulse_dashboard.dart
+++ lib/screens/pulse_dashboard.dart
@@ -277,7 +277,7 @@ class _PulseDashboardState extends State<PulseDashboard> {
 
           const SizedBox(height: 5),
           const Text(
-            "Version 1.0.5",
+            "Version 1.0.6",
 
             style: TextStyle(
               fontSize: 8,
